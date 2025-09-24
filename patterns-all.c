fb495788c81a0619d0530d80dec85c8e700f6241
nandanirangani50-crypto <nandanirangani50@gmail.com>
2025-09-24 17:54:25 +0530

    Add files via upload

---
+++ patterns-all.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+
+int main()
+{
+    // for(int i=1; i<=5;i++){                 1
+    //     for(int j=1;j<=i;j++){              12
+    //         printf("%d",j);                 123
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    
+    // for(int i=1; i<=5;i++){               1
+    //     for(int j=1;j<=i;j++){            22
+    //         printf("%d",i);               333
+    //     }
+    //     printf("\n");
+    // }
+
+    
+    
+    // for(char i='a'; i<='e';i++){         a
+    //     for(int j='a';j<=i;j++){         ab
+    //         printf("%c",j);              abc
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    // for(char i='a'; i<='e';i++){            a
+    //     for(int j='a';j<=i;j++){            bb
+    //         printf("%c",i);                 ccc
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    
+    
+    // for(int i=1; i<=5;i++){                 *
+    //     for(int j=1;j<=i;j++){              **
+    //         printf("*");                    ***
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    //   for(int i=1; i<=5;i++){                  *
+    //       for(int k=5;k>=i;k--){              **
+    //           printf(" ");                   ***
+    //       }
+    //     for(int j=1;j<=i;j++){              
+    //         printf("*");                
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    //       for(int i=5; i>=1;i--){            12345
+    //       for(int k=5;k>=i;k--){              1234
+    //           printf(" ");                     123
+    //       }
+    //     for(int j=1;j<=i;j++){              
+    //         printf("%d",j);                
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    
+    // for(int i=1; i<=5;i++){                   1
+    //       for(int k=5;k>=i;k--){             12
+    //           printf(" ");                  123
+    //       }                                1234
+    //     for(int j=1;j<=i;j++){              
+    //         printf("%d",j);                
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    
+    
+    //  for(int i=5; i>=1;i--){            12345
+    //     for(int j=1;j<=i;j++){          1234
+    //         printf("%d",j);             123
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    //  for(char i='a'; i<='e';i++){             a
+    //      for(char k=i;k<='e';k++){           ab
+    //          printf(" ");                   abc
+    //      }                                 abcd
+    //     for(int j='a';j<=i;j++){          
+    //         printf("%c",j);             
+    //     }
+    //     printf("\n");
+    // }
+    
+
+    //   for(char i='a'; i<='e';i++){            a 
+    //      for(char k=i;k<='e';k++){            bb
+    //          printf(" ");                    ccc
+    //      }                                  dddd
+    //     for(int j='a';j<=i;j++){          
+    //         printf("%c",i);             
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    return 0;
+}
